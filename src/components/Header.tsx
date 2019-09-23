9f8dc2aa89d9822b0aadd5a7fa6e0e85a8038473
Shota Shimizu <shota.shimizu226@gmail.com>
2019-09-24 02:04:53 +0900

    検索欄の表示

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-import {createStyles, makeStyles, Theme} from "@material-ui/core";
+import {createStyles, fade, makeStyles, Theme} from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import {GITLAB_HEADER_COLOR} from "../constants";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -10,6 +13,50 @@ const useStyles = makeStyles((theme: Theme) =>
             backgroundColor: GITLAB_HEADER_COLOR,
             flexGrow: 1,
             top: 0,
+        },
+        title: {
+            flexGrow: 1,
+            display: 'none',
+            [theme.breakpoints.up('sm')]: {
+                display: 'block',
+            },
+        },
+        search: {
+            position: 'relative',
+            borderRadius: theme.shape.borderRadius,
+            backgroundColor: fade(theme.palette.common.white, 0.15),
+            '&:hover': {
+                backgroundColor: fade(theme.palette.common.white, 0.25),
+            },
+            marginLeft: 0,
+            width: '100%',
+            [theme.breakpoints.up('sm')]: {
+                marginLeft: theme.spacing(1),
+                width: 'auto',
+            },
+        },
+        searchIcon: {
+            width: theme.spacing(7),
+            height: '100%',
+            position: 'absolute',
+            pointerEvents: 'none',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        inputRoot: {
+            color: 'inherit',
+        },
+        inputInput: {
+            padding: theme.spacing(1, 1, 1, 7),
+            transition: theme.transitions.create('width'),
+            width: '100%',
+            [theme.breakpoints.up('sm')]: {
+                width: 200,
+                '&:focus': {
+                    width: 250,
+                },
+            },
         },
     }),
 );
@@ -19,7 +66,25 @@ const Header: React.FC = () => {
 
     return (
         <AppBar position="fixed" className={classes.root}>
-            <Toolbar variant="dense"/>
+            <Toolbar variant="dense">
+                <Typography variant="h6" noWrap className={classes.title}>
+                    CRALY Backlog Viewer
+                </Typography>
+                <div className={classes.search}>
+                    <div className={classes.searchIcon}>
+                        <SearchIcon/>
+                    </div>
+                    <InputBase
+                        disabled
+                        placeholder="Search…"
+                        classes={{
+                            root: classes.inputRoot,
+                            input: classes.inputInput,
+                        }}
+                        inputProps={{'aria-label': 'search'}}
+                    />
+                </div>
+            </Toolbar>
         </AppBar>
     )
 };
